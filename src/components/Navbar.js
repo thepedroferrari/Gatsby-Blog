751b44a6ed29fd8187041e35fd93aa8b6ae82e36
thepedroferrari <pedro@pedroferrari.com>
2018-07-03 00:46:38 +0200

    Updating Dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Kaldi" style={{width: '88px'}} />
           </figure>
         </Link>
       </div>
@@ -19,13 +19,13 @@ const Navbar = () => (
           About
         </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          CV
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/thepedroferrari/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -36,6 +36,6 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
